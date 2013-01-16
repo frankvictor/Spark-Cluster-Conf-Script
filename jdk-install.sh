1d2408071693a04ddf4f1138b6f438cc5eb32cfc
frank <frankvictor@qq.com>
2013-01-16 16:35:09 +0800

    updat install_jdk.sh

--- jdk-install.sh
+++ jdk-install.sh
@@ -4,10 +4,12 @@
 #  1.now can install arbitrary version
 #  2.now can install in arbitrary path
 
-if [ "$#" -ne "1" ]; then
-	echo "usage $0 jdk_file-path install_path"
+if [ "$#" -ne "2" ]; then
+	echo "usage $0 jdk_file-path install_path(do not need end with /)"
+	echo "need to use other method to add java to PATH"
 	echo "can install jdk on remote machines"
-	echo "example: $0 ./jdk-6u31-linux-x64.bin soft/(can not be ~/soft)"
+	echo "file type can be: bin or tar.gz"
+	echo "example: $0 ./jdk-6u31-linux-x64.bin soft(can not be ~/soft)"
 	exit 1
 fi
 
@@ -21,14 +23,18 @@ if [ -f $1 ]; then
 			read end
 
 			jdk_file_name=`basename $1`
-			echo "jdk_file_name: $jdk_file_name"
 
 			while [ $startd -le $end ]
 			do
-				ssh $prefix$startd mkdir -p $2 
+				ssh $prefix$startd mkdir -p $2
 				scp $1 $prefix$startd:~/$2
-				ssh $prefix$startd "cd $2 && ./$jdk_file_name && rm $jdk_file_name
+				if [ ${jdk_file_name##*.} == "bin" ]; then
+					ssh $prefix$startd "cd $2 && chmod +x ./$jdk_file_name && ./$jdk_file_name && rm $jdk_file_name"
+				else
+					ssh $prefix$startd "cd $2 && tar zxf ./$jdk_file_name && rm $jdk_file_name"
+				fi
 				((startd++))
+
 			done
 else
 	echo "file $1 does not exist!"
